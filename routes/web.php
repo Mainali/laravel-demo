cd9d3218e1d005c6aecbbfe26524ede18a8632c8
Pukar Mainali <mainalipukar@gmail.com>
2017-08-13 12:10:07 -0500

    add routes for product

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/products','ProductController@index')->name('products.index');
+Route::get('/products/create','ProductController@create')->name('products.create');
+Route::get('/products/store','ProductController@store')->name('products.store');
